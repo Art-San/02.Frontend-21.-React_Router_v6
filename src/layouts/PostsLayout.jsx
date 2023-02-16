d3d3abba95f31e5fb42ff24c53dee7271622d96f
Aleksander <asava@sibmail.com>
2023-02-16 14:32:02 +0700

    Outlet и Layouts

--- src/layouts/PostsLayout.jsx
+++ src/layouts/PostsLayout.jsx
@@ -1,13 +1,14 @@
 import React, { useEffect } from "react";
 // Librares
 import { useDispatch } from "react-redux";
-import { Routes, Route } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+// import { Routes, Route } from "react-router-dom";
 // Components
 import BackgroundGradiend from "../components/BackgroundGradient";
 import ScreenWidthWrapper from "../components/ScreenWidthWrapper";
 // Pages
-import PostPage from "../pages/Posts/PostPage";
-import PostsListPage from "../pages/Posts/PostsListPage";
+// import PostPage from "../pages/Posts/PostPage";
+// import PostsListPage from "../pages/Posts/PostsListPage";
 //Store
 import { getPosts } from "../store/postsSlice";
 
@@ -26,11 +27,12 @@ const PostsLayout = ({ children }) => {
         <>
             <BackgroundGradiend />
             <ScreenWidthWrapper>
-                <Routes>
+                <Outlet/>
+                {/* <Routes>
                     <Route path={":postId"} element={<PostPage/>} />
                     <Route path={''} element={<PostsListPage/>} />
-                </Routes>
-                {children}
+                </Routes> */}
+                {/* {children} */}
             </ScreenWidthWrapper>
         </>
     );
